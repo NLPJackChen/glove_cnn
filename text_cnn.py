73ecb522f9969be5228ec7f23d3241bb4bcb2c61
NLPJackChen <30566790+NLPJackChen@users.noreply.github.com>
2018-07-26 14:42:08 +0800

    Update text_cnn.py

--- text_cnn.py
+++ text_cnn.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 
-
 class TextCNN(object):
     '''
     A CNN for text classification
@@ -9,7 +8,7 @@ class TextCNN(object):
     '''
     def __init__(self, sequence_length, num_classes,embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
 
-            # Placeholders for input, output, dropout
+        # Placeholders for input, output, dropout
         self.input_x = tf.placeholder(tf.float32, [None, sequence_length, embedding_size], name = "input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name = "input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name = "dropout_keep_prob")
@@ -18,31 +17,17 @@ class TextCNN(object):
         l2_loss = tf.constant(0.0)
 
         # Embedding layer
-            # self.embedded_chars = [None(batch_size), sequence_size, embedding_size]
-            # self.embedded_chars = [None(batch_size), sequence_size, embedding_size, 1(num_channels)]
         self.embedded_chars = self.input_x
-        self.embedded_chars_expended = tf.expand_dims(self.embedded_chars, -1)#[2,186,128,1]
-
-        # 't' is a tensor of shape [2]
-        # shape(expand_dims(t, 0)) == > [1, 2]
-        # shape(expand_dims(t, 1)) == > [2, 1]
-        # shape(expand_dims(t, -1)) == > [2, 1]
-        # # 't2' is a tensor of shape [2, 3, 5]
-        # shape(expand_dims(t2, 0)) == > [1, 2, 3, 5]
-        # shape(expand_dims(t2, 2)) == > [2, 3, 1, 5]
-        # shape(expand_dims(t2, 3)) == > [2, 3, 5, 1]
+        self.embedded_chars_expended = tf.expand_dims(self.embedded_chars, -1)
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
         for i, filter_size in enumerate(filter_sizes):
-            # print (filter_sizes)[3,4,5]
-            # print (i)
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 # Convolution layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")#[3,128,1,100]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                conv = tf.nn.conv2d(self.embedded_chars_expended,W,strides=[1,1,1,1],padding="VALID",name="conv")#(2, 184, 1, 100)
-
+                conv = tf.nn.conv2d(self.embedded_chars_expended,W,strides=[1,1,1,1],padding="VALID",name="conv")
                 # Apply nonlinearity
                 h = tf.nn.relu(tf.nn.bias_add(conv, b), name = "relu")
                 # Maxpooling over the outputs
